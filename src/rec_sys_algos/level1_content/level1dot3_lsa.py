13eb21662cbf7b50bf18f1fd56c081c936df2ba5
Karthik Ravi <karthik.ravi.1537@gmail.com>
2025-04-08 13:37:40 -0700

    Refactoring the code.

--- src/rec_sys_algos/level1_content/level1dot3_lsa.py
+++ src/rec_sys_algos/level1_content/level1dot3_lsa.py
@@ -18,7 +18,7 @@ def compute_embeddings(texts, n_components=100):
     return svd.fit_transform(tfidf_matrix)
 
 
-@cache_results("item_profiles_lsa_cache.pkl", force_recompute=False)
+@cache_results("lsa_item_profiles_cache.pkl", force_recompute=False)
 def build_item_profiles(business_df, reviews_df):
     """Build item profiles using LSA embeddings."""
     aggregated_reviews = base.aggregate_reviews(reviews_df)
@@ -29,7 +29,7 @@ def build_item_profiles(business_df, reviews_df):
 
 
 def content_based_recommendations(user_id, ratings_df, profiles, top_n=5):
-    base.content_based_recommendations(user_id, ratings_df, profiles, top_n)
+    return base.content_based_recommendations(user_id, ratings_df, profiles, top_n)
 
 
 if __name__ == "__main__":
